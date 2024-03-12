e6b74168ec0b78d9c7cd90de6038cafd4acf990f
Nhân_Cùi_GòZấp <phamducnhan.it@gmail.com>
2024-03-12 23:32:30 +0700

    up gateway , loginJWT, Redis

---
+++ AuthService/src/main/java/com/example/AuthService/valid/RouterValid.java
@@ -0,0 +1,16 @@
+package com.example.AuthService.valid;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RouterValid {
+	public static final List<String> endpointsNoAuth = List.of("auth/register", "auth/login");
+	
+	public Predicate<ServerHttpRequest> isSecured = request  -> endpointsNoAuth.stream()
+			.noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
